faa8762840dbc1dd217c113458c323c37ae7c90e
RomainTellier <romain.tellier@sylpheo.com>
2017-11-10 14:13:18 +0100

    ADD Working routes

--- src/RT/PlatformBundle/Controller/AdvertController.php
+++ src/RT/PlatformBundle/Controller/AdvertController.php
@@ -4,10 +4,35 @@ namespace RT\PlatformBundle\Controller;
 
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 
 class AdvertController extends Controller
 {
+
+    // La route fait appel à OCPlatformBundle:Advert:view,
+    // on doit donc définir la méthode viewAction.
+    // On donne à cette méthode l'argument $id, pour
+    // correspondre au paramètre {id} de la route
+    public function viewAction($id, Request $request)
+    {
+        // On récupère notre paramètre tag
+        $tag = $request->query->get('tag');
+
+        return new Response(
+            "Affichage de l'annonce d'id : ".$id.", avec le tag : ".$tag
+        );
+    }
+
+    // On récupère tous les paramètres en arguments de la méthode
+    public function viewSlugAction($slug, $year, $format)
+    {
+        return new Response(
+            "On pourrait afficher l'annonce correspondant au
+            slug '".$slug."', créée en ".$year." et au format ".$format."."
+        );
+    }
+
 	public function indexAction()
 	{
 		$content = $this->get('templating')->render('RTPlatformBundle:Advert:index.html.twig', array('nom' => 'RomainTheBOSS'));
