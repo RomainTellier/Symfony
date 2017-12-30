0508ff8f979e73ed02629402ac68ae5bf7103fc0
RomainTellier <romain.tellier@sylpheo.com>
2017-12-30 22:33:17 +0100

    Ajout d'un champ test name pour l'inscription au site, totalement fonctionelle,todo:ajouter les autres champs!. Ajout également de la traduction en locale FR

---
+++ src/RT/PlatformBundle/Form/RegistrationType.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Romain
+ * Date: 30/12/2017
+ * Time: 22:09
+ */
+
+namespace RT\PlatformBundle\Form;
+
+    use FOS\UserBundle\Util\LegacyFormHelper;
+    use Symfony\Component\Form\AbstractType;
+    use Symfony\Component\Form\FormBuilderInterface;
+    use Symfony\Component\OptionsResolver\OptionsResolver;
+
+class RegistrationType extends AbstractType
+{
+
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder->add('name');
+    }
+
+    public function getParent()
+    {
+        return 'FOS\UserBundle\Form\Type\RegistrationFormType';
+    }
+
+    public function getBlockPrefix()
+    {
+        return 'app_user_registration';
+    }
+
+    // For Symfony 2.x
+    public function getName()
+    {
+        return $this->getBlockPrefix();
+    }
+}
