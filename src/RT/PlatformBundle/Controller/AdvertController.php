d7634b02dd5b187b6734aa7317166a71664865ce
RomainTellier <romain.tellier@sylpheo.com>
2018-01-16 17:19:47 +0100

    Fix some minimal bug, refacto code before release

--- src/RT/PlatformBundle/Controller/AdvertController.php
+++ src/RT/PlatformBundle/Controller/AdvertController.php
@@ -159,12 +159,6 @@ class AdvertController extends Controller
             }
         }
 
-        // À ce stade, le formulaire n'est pas valide car :
-        // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
-        // - Soit la requête est de type POST, mais le formulaire contient des valeurs invalides, donc on l'affiche de nouveau
-
-        // On passe la méthode createView() du formulaire à la vue
-        // afin qu'elle puisse afficher le formulaire toute seule
         // on pase les objets
         return $this->render('RTPlatformBundle:Advert:view.html.twig', array(
             'theme' => $theme,
@@ -192,7 +186,6 @@ class AdvertController extends Controller
             ->add('titre',     TextType::class)
             ->add('Enregistrer',      SubmitType::class)
         ;
-        // Pour l'instant, pas de candidatures, catégories, etc., on les gérera plus tard
 
         // À partir du formBuilder, on génère le formulaire
         $form = $formBuilder->getForm();
@@ -219,81 +212,13 @@ class AdvertController extends Controller
         }
         $user = $this->getUser();
 
-        // À ce stade, le formulaire n'est pas valide car :
-        // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
-        // - Soit la requête est de type POST, mais le formulaire contient des valeurs invalides, donc on l'affiche de nouveau
+
         return $this->render('RTPlatformBundle:Advert:add.html.twig', array(
             'form' => $form->createView(),
             'user' => $user,
         ));
-        // On passe la méthode createView() du formulaire à la vue
-        // afin qu'elle puisse afficher le formulaire toute seule
-
-
     }
 
-    /*public function addDiscussionAction($id, Request $request)
-    {
-        // On récupère le repository
-        $repository = $this->getDoctrine()
-            ->getManager()
-            ->getRepository('RTPlatformBundle:Theme')
-        ;
-        // On récupère l'entité correspondante à l'id $id
-        $theme = $repository->find($id);
-
-        $discussion = new Discussion();
-
-        // On crée le FormBuilder grâce au service form factory
-        $formBuilder = $this->get('form.factory')->createBuilder(FormType::class, $discussion);
-
-        // On ajoute les champs de l'entité que l'on veut à notre formulaire
-        $formBuilder
-            ->add('date',         DateType::class)
-            ->add('theme_id',     NumberType::class)
-            ->add('heure',        TimeType::class)
-            ->add('pseudo',       TextType::class)
-            ->add('content',      TextType::class)
-            ->add('save',         SubmitType::class)
-        ;
-
-        // À partir du formBuilder, on génère le formulaire
-        $form = $formBuilder->getForm();
-
-        // Si la requête est en POST
-        if ($request->isMethod('POST')) {
-            // On fait le lien Requête <-> Formulaire
-            // À partir de maintenant, la variable $discussion contient les valeurs entrées dans le formulaire par le visiteur
-            $form->handleRequest($request);
-
-            // On vérifie que les valeurs entrées sont correctes
-            // (Nous verrons la validation des objets en détail dans le prochain chapitre)
-            if ($form->isValid()) {
-                // On enregistre notre objet $discussion dans la base de données, par exemple
-                $em = $this->getDoctrine()->getManager();
-                $em->persist($discussion);
-                $em->flush();
-
-                $request->getSession()->getFlashBag()->add('notice', 'Discussion bien enregistrée !');
-
-                // On redirige vers la page de visualisation de l'annonce nouvellement créée
-                return $this->redirectToRoute('rt_platform_view', array('id' => $theme->getId()));
-            }
-        }
-        $user = $this->getUser();
-
-        // À ce stade, le formulaire n'est pas valide car :
-        // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
-        // - Soit la requête est de type POST, mais le formulaire contient des valeurs invalides, donc on l'affiche de nouveau
-        return $this->render('RTPlatformBundle:Advert:addDiscussion.html.twig', array(
-            'form' => $form->createView(),
-            'user' => $user,
-        ));
-        // On passe la méthode createView() du formulaire à la vue
-        // afin qu'elle puisse afficher le formulaire toute seule
-
-
-    }*/
 
     public function editAction($id, Request $request)
     {
@@ -348,8 +273,6 @@ class AdvertController extends Controller
             'theme' => $theme,
             'user' => $user,
         ));
-        // On passe la méthode createView() du formulaire à la vue
-        // afin qu'elle puisse afficher le formulaire toute seule
 
     }
 
@@ -403,16 +326,12 @@ class AdvertController extends Controller
         }
         $user = $this->getUser();
 
-        // À ce stade, le formulaire n'est pas valide car :
-        // - Soit la requête est de type GET, donc le visiteur vient d'arriver sur la page et veut voir le formulaire
-        // - Soit la requête est de type POST, mais le formulaire contient des valeurs invalides, donc on l'affiche de nouveau
+
         return $this->render('RTPlatformBundle:Advert:editDiscussion.html.twig', array(
             'form' => $form->createView(),
             'theme' => $theme,
             'user' => $user,
         ));
-        // On passe la méthode createView() du formulaire à la vue
-        // afin qu'elle puisse afficher le formulaire toute seule
 
     }
 
@@ -518,88 +437,3 @@ class AdvertController extends Controller
 
     }
 }
-
-//class AdvertController extends Controller
-//{
-//
-//    // La route fait appel à RTPlatformBundle:Advert:view,
-//    // on doit donc définir la méthode viewAction.
-//    // On donne à cette méthode l'argument $id, pour
-//    // correspondre au paramètre {id} de la route
-//    public function viewAction($id, Request $request)
-//    {
-//        // Récupération de la session
-//        /*$session = $request->getSession();
-//
-//        // On récupère le contenu de la variable user_id
-//        $userId = $session->get('user_id');
-//
-//        // On définit une nouvelle valeur pour cette variable user_id
-//        $session->set('user_id', 91);
-//
-//        // On n'oublie pas de renvoyer une réponse
-//        return new Response("<body>Je suis une page de test, je n'ai rien à dire</body>");*/
-//
-//        // On récupère notre paramètre tag
-//        $tag = $request->query->get('tag');
-//
-//        /*return new Response(
-//            "Affichage de l'annonce d'id : ".$id.", avec le tag : ".$tag
-//        );*/
-//
-//        // On utilise le raccourci : il crée un objet Response
-//        // Et lui donne comme contenu le contenu du template
-//        /*return $this->render('RTPlatformBundle:Advert:view.html.twig', array(
-//            'id'  => $id,
-//            'tag' => $tag,
-//        ));*/
-//
-//        // return new JsonResponse(array('id' => $id));
-//
-//        // return $this->redirectToRoute('rt_platform_home');
-//
-//        return $this->render('RTPlatformBundle:Advert:view.html.twig', array(
-//            'id' => $id
-//        ));
-//    }
-//
-//    // On récupère tous les paramètres en arguments de la méthode
-//    public function viewSlugAction($slug, $year, $format)
-//    {
-//        return new Response(
-//            "On pourrait afficher l'annonce correspondant au
-//            slug '".$slug."', créée en ".$year." et au format ".$format."."
-//        );
-//    }
-//
-//	public function indexAction()
-//	{
-//		$content = $this->get('templating')->render('RTPlatformBundle:Advert:index.html.twig', array('nom' => 'RomainTheBOSS'));
-//
-//		return new Response($content);
-//	}
-//
-//	public function page2Action()
-//	{
-//		$content = $this->get('templating')->render('RTPlatformBundle:Advert:page2.html.twig', array('nom' => 'RomainLekéké'));
-//
-//		return new Response($content);
-//	}
-//
-//    public function addAction(Request $request)
-//    {
-//        $session = $request->getSession();
-//
-//        // Bien sûr, cette méthode devra réellement ajouter l'annonce
-//
-//        // Mais faisons comme si c'était le cas
-//        $session->getFlashBag()->add('info', 'Annonce bien enregistrée');
-//
-//        // Le « flashBag » est ce qui contient les messages flash dans la session
-//        // Il peut bien sûr contenir plusieurs messages :
-//        $session->getFlashBag()->add('info', 'Oui oui, elle est bien enregistrée !');
-//
-//        // Puis on redirige vers la page de visualisation de cette annonce
-//        return $this->redirectToRoute('rt_platform_view', array('id' => 5));
-//    }
-//}
